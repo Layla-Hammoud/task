b2c437a9b4f8139c62b11509bbb02ba2d430c112
Layla Hammoud <laylahammoud63@gmail.com>
2023-02-26 17:40:03 +0200

    finish the prime number page

---
+++ prime.js
@@ -0,0 +1,37 @@
+const prime_text = document.getElementById("prime");
+let prime=true
+function Input() {
+    let age = parseInt(prompt("Enter your age must be positive integer"));
+    console.log(age)
+    while (!Number.isInteger(age)) {
+        age = parseInt(prompt("Enter your age must be a positive integer"));
+    }
+    isPrime(age)
+    console.log(prime)
+    if(prime){
+        prime_text.innerHTML=`your age is a prime number`
+
+    }
+    else{
+        prime_text.innerHTML=`your age is not a prime number`
+    }
+}
+
+function isPrime(age) {
+    if (age > 1) {
+        for (let i = 2; i < age; i++) {
+            if (age % i == 0) {
+                prime = false;
+                break;
+            }
+        }
+
+    }
+}
+
+
+
+
+
+
+Input()
