b6718d7e87f317d71a8a1959d34ec1e95f3d5e4a
Layla Hammoud <laylahammoud63@gmail.com>
2023-02-26 20:21:19 +0200

    get the sum of  even number from the ip

--- ip.js
+++ ip.js
@@ -1,5 +1,18 @@
-let ip=""
-fetch('https://api.ipify.org/?format=json').then(
-  result => result.json()
-).then(data=>{ip = data.ip} );
-console.log(ip)
+let ip = ""
+let sum = 0
+async function getip() {
+    let array_IP = []
+    const result = await fetch('https://api.ipify.org/?format=json')
+    ip = await result.json();
+    ip = ip.ip
+    array_IP = ip.split(".")
+    console.log(array_IP)
+    for (let i = 0; i < array_IP.length; i++) {
+        if (array_IP[i] % 2 == 0) {
+            sum += array_IP[i]
+
+        }
+    }
+    return array_IP
+}
+getip()
